6eba33614d0b18ccfc5b51d3ee518a5a5751bcb1
Sebastian Wellgren Lax <161583284+SebastianWellgrenLax@users.noreply.github.com>
2024-04-18 14:23:43 +0200

    Add files via upload

---
+++ main/main.cpp
@@ -0,0 +1,16 @@
+#include "ObjectDetection.hpp"
+
+int main() {
+    /*
+    std::string imgPath = "C:/Users/Sebastian WL/Desktop/Images/stop.jpg";
+
+    ObjectDetection objectDetection();
+
+    cv::Mat image = objectDetection.readImage(imgPath);
+
+    objectDetection.identifyObjectTest(image);
+
+    return 0;
+    */
+}
+
