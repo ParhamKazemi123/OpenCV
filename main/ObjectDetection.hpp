1a84e2c58389a7a9923fdc58fead3493e41deb16
Sebastian Wellgren Lax <161583284+SebastianWellgrenLax@users.noreply.github.com>
2024-04-29 11:38:24 +0200

    Add files via upload

--- main/ObjectDetection.hpp
+++ main/ObjectDetection.hpp
@@ -1,5 +1,9 @@
+#ifndef OBJECTDETECTION_HPP
+#define OBJECTDETECTION_HPP
+
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include "ObjectInfo.hpp"
 using namespace cv;
 
 class ObjectDetection {
@@ -11,18 +15,10 @@ public:
     cv::Mat findObject(cv::Mat image, int x, int y);
     int findObjectArea(cv::Mat image, int x, int y);
 
-    cv::Mat identifyColor(cv::Mat image);
-    cv::Mat identifyColor(cv::Mat image, int x, int y);
-    cv::Mat identifyAllObjects(cv::Mat& image, int invert);
-    int identifyAllObjectAreas(cv::Mat& image, int invert);
-    void removeBackground(cv::Mat& image);
-    cv::Mat readImage(const std::string& imgPath);
+    ObjectInfo findObjectInfo(cv::Mat image, int x, int y);
 
 private:
-    int contourThickness(cv::Mat image);
-    std::vector<std::vector<cv::Point>> getContours(cv::Mat& image, int invert, int retr);
-    int getAverageHSV(const cv::Mat& image, int x, int y);
-    int getHSV(const cv::Mat image);
-    cv::Mat createMask(const cv::Mat& inputImage, const cv::Scalar& lowerBound, const cv::Scalar& upperBound);
-    cv::Mat applyMask(const cv::Mat& image, const cv::Mat& mask);
+    std::vector<std::vector<cv::Point>> getContours(cv::Mat& image);
 };
+
+#endif // OBJECTDETECTION_HPP
--- main/main.cpp
+++ main/main.cpp
@@ -1,16 +1,25 @@
+#include <opencv2/opencv.hpp>
 #include "ObjectDetection.hpp"
+#include "ObjectInfo.hpp"
+using namespace cv;
 
 int main() {
-    /*
-    std::string imgPath = "C:/Users/Sebastian WL/Desktop/Images/stop.jpg";
+    std::string imgPath = "C:/Users/Sebastian WL/Desktop/Images/blood.jpg";
 
-    ObjectDetection objectDetection();
+    cv::Mat image = cv::imread(imgPath, cv::IMREAD_COLOR);
 
-    cv::Mat image = objectDetection.readImage(imgPath);
+    ObjectDetection detection;
 
-    objectDetection.identifyObjectTest(image);
+    ObjectInfo info = detection.findObjectInfo(image, 100, 100);
+
+    // Print area and center points
+    std::cout << "Area: " << info.area << std::endl;
+    std::cout << "Center Point: (" << info.center.x << ", " << info.center.y << ")" << std::endl;
+
+    // Display the image
+    cv::imshow("Image with Contours", info.image);
+    cv::waitKey(0); // Wait for a key press before closing the window
 
     return 0;
-    */
 }
 
